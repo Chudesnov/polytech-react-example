f829b80e665365014dd5c60db2a0677e8f84c3d6
Alexander Chudesnov <a.chudesnov@tinkoff.ru>
2017-10-06 16:13:47 +0300

    some changes

--- src/ExampleComponent.js
+++ src/ExampleComponent.js
@@ -1,64 +1,65 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import localStored from './decorators/localStored';
+import React from "react";
+import PropTypes from "prop-types";
+import localStored from "./decorators/localStored";
 
 class ExampleComponent extends React.Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            value: ''
-        }
-        this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      value: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  componentWillMount() {}
+  componentDidMount() {
+    if (this.props.initialValue && !this.state.value) {
+      this.input.value = this.props.initialValue;
+      this.setState({
+        value: this.props.initialValue
+      });
     }
-    componentWillMount() {
-    }
-    componentDidMount() {
-        if (this.props.initialValue && !this.state.value) {
-            this.input.value = this.props.initialValue;
-            this.setState({
-                value: this.props.initialValue
-            });
-        }
-    }
-    componentWillReceiveProps(nextProps) {
-        nextProps;
-    }
-    componentWillUpdate(nextProps, nextState) {
-        this.props.smth !== nextProps.smth && alert('omg');
-    }
-    componentDidUpdate() {
-        this.input.value;
-    }
-    componentWillUnmount() {
-
-    }
-    handleChange(event) {
-        this.setState({
-            value: event.target.value
-        });
-        this.props.onChange(event.target.value);
-    }
-    render() {
-        return <div>
-            <input
-                ref={inputElement => { this.input = inputElement; }}
-                onChange={this.handleChange}
-            />
-            <p>{this.state.value}</p>
-        </div>;
-    }
+  }
+  componentWillReceiveProps(nextProps) {
+    nextProps;
+  }
+  componentWillUpdate(nextProps, nextState) {
+    this.props.smth !== nextProps.smth && alert("omg");
+  }
+  componentDidUpdate() {
+    this.input.value;
+  }
+  componentWillUnmount() {}
+  handleChange(event) {
+    this.setState({
+      value: event.target.value + "stored"
+    });
+    this.props.onChange(event.target.value);
+  }
+  render() {
+    return (
+      <div>
+        <input
+          ref={inputElement => {
+            this.input = inputElement;
+          }}
+          onChange={this.handleChange}
+        />
+        <p>{this.state.value}</p>
+      </div>
+    );
+  }
 }
 
 ExampleComponent.propTypes = {
-    initialValue: PropTypes.string,
-    onChange: PropTypes.func
-}
+  initialValue: PropTypes.string,
+  onChange: PropTypes.func
+};
 
 ExampleComponent.defaultProps = {
-    onChange: () => {}
-}
+  onChange: () => {}
+};
 
-const localStoredExample = localStored('example', ExampleComponent);
+const localStoredExample = localStored("test", ExampleComponent);
 
 export default localStoredExample;
--- src/decorators/localStored.js
+++ src/decorators/localStored.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React from "react";
 
 export default function(storageKey, ComponentClass) {
-    return class LocalStored extends React.Component {
-        constructor(props) {
-            super(props);
-            this.setLocalStorageItem = this.setLocalStorageItem.bind(this);
-        }
-        componentWillMount() {
-            this.setState({
-                initialValue: localStorage.getItem(storageKey)
-            });
-        }
-        setLocalStorageItem(value) {
-            localStorage.setItem(storageKey, value)
-        }
-        render() {
-            return <ComponentClass
-                {...this.props}
-                onChange={this.setLocalStorageItem}
-                initialValue={this.state.initialValue}
-            />
-        }
+  return class LocalStored extends React.Component {
+    constructor(props) {
+      super(props);
+      this.setLocalStorageItem = this.setLocalStorageItem.bind(this);
+      console.log(storageKey);
     }
+    componentWillMount() {
+      this.setState({
+        initialValue: localStorage.getItem(storageKey)
+      });
+    }
+    setLocalStorageItem(value) {
+      localStorage.setItem(storageKey, value);
+    }
+    render() {
+      return (
+        <ComponentClass
+          {...this.props}
+          onChange={this.setLocalStorageItem}
+          initialValue={this.state.initialValue}
+        />
+      );
+    }
+  };
 }
